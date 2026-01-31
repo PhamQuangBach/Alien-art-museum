c5ce6f0a92f453e0d34b5776349ab8d88366b9d4
Bach <bachpq2003@gmail.com>
2026-01-31 15:25:21 +0200

    Input system

--- Assets/Scripts/BeatHitter.cs
+++ Assets/Scripts/BeatHitter.cs
@@ -1,23 +1,38 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BeatHitter : MonoBehaviour
 {
     private beat beatScript;
-    private AudioManager audioManager; 
+    private AudioManager audioManager;
+
+    private InputAction[] inputActions = new InputAction[4];
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       beatScript = GameObject.Find("beat").GetComponent<beat>(); 
-       audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+        beatScript = GameObject.Find("beat").GetComponent<beat>();
+        audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+
+        inputActions[0] = InputSystem.actions.FindAction("Up");
+        inputActions[1] = InputSystem.actions.FindAction("Right");
+        inputActions[2] = InputSystem.actions.FindAction("Down");
+        inputActions[3] = InputSystem.actions.FindAction("Left");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < 4; i++)
+        {
+            if (inputActions[i].WasPerformedThisFrame())
+            {
+                InputPressed(i);
+            }
+        }
     }
 
-    public void InputPressed()
+    public void InputPressed(int input)
     {
         Debug.Log("input pressed!");
 
@@ -29,8 +44,8 @@ public class BeatHitter : MonoBehaviour
     {
         if (beatScript.IsOnBeat())
         {
-            audioManager.PlaySound("Happy",1,1.2f);
+            audioManager.PlaySound("Happy", 1, 1.2f);
         }
-        
+
     }
 }
